937f505d92e714f8acc76b0534235f96544d9ad7
xwHan0 <xwhan2015@outlook.com>
2018-09-28 14:42:07 +0800

    Add title description

--- kldMgr/node.py
+++
@@ -1,7 +0,0 @@
-class BasicCell():
-    def __init__(self, name, x = 0, y = 0, href='', style=''):
-        self.name = name
-        self.x = x
-        self.y = y
-        self.href = href
-        self.style = style if style != '' else 'doc'
