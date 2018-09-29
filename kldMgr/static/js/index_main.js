20eaaae5481dd827c23ef57501eb0b611c4f7083
xwHan0 <xwhan2015@outlook.com>
2018-09-29 15:37:50 +0800

    fix bug and add toggle

--- kldMgr/static/js/index_main.js
+++ kldMgr/static/js/index_main.js
@@ -4,8 +4,9 @@ alert("hellow, world!")
 $(document).ready(function(){
 
     $("image").on("click", function(e){
-        gele = $(e.target).parent()
-        alert($(gele).attr("id"))
+        gele = $(e.target).parent().parent()
+        gele = $(gele).children("g.comment")
+        gele.toggle()
     })
 
 })
