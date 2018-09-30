7f194b22eb5c5cc940214800bc0b4df4af75fe89
xwHan0 <xwhan2015@outlook.com>
2018-09-30 09:36:53 +0800

    add event

--- kldMgr/static/js/index_main.js
+++ kldMgr/static/js/index_main.js
@@ -1,12 +1,13 @@
-alert("hellow, world!")
+//alert("hellow, world!")
 
 
 $(document).ready(function(){
 
     $("image").on("click", function(e){
-        gele = $(e.target).parent().parent()
-        gele = $(gele).children("g.comment")
-        gele.toggle()
+        gele = $(e.target).parent()
+        detailName = '#' + gele.attr('id') + '_detail'
+        $(detailName).toggle()
+        $(gele).children("image#expand_btn").toggle()
     })
 
 })
