31435a9c5e71d204712cda4d014d37bc0f9723e4
xwHan0 <xwhan2015@outlook.com>
2018-10-09 11:11:19 +0800

    add statement index.svg

--- kldMgr/db/readcell.py
+++ kldMgr/db/readcell.py
@@ -5,17 +5,35 @@ def read_cell():
 
   conn = sqlite3.connect('static/database/index.db3')
   conn.row_factory = sqlite3.Row
-  c = conn.cursor()
-  c.execute('SELECT * FROM knowledge')
+  items = conn.execute('SELECT * FROM knowledge')
 
+  rst = []
   lastCell = BasicCell()  # 设置初始Cell
-  cells = [BasicCell(k['id'],k['']) for k in c]
-  for item in cells:
-    # 设置
-    key0 = item['id']
-    # 查找一级子分支
+  for item in items:
+      cell = BasicCell(
+          name = item['name'],
+          href = item['href'],
+          title = item['title'],
+          style = item['style'],
+          ofstx = item['x'],
+          ofsty = item['y'],
+          refcell = last_cell
+      )
+      key0 = item['id']
+      brach0 = conn.execute('SELECT * FROM detail WHERE parent=? AND lvl=?', (key0,1))
+      for brach in brach0:
+          b = BasicBrach(brach['name'], brach['href'])
+          key1 = brach['id']
+          content = conn.execute('SELECT * FROM detail WHERE parent=? AND lvl=?', (key1,2))
+          b.subs = [BasicBrach(name=c['name'], href=c['href']) for c in content]
+          cell.subs.append(b)
+       cell.subs = cells.subs if cell.subs != [] else None
+       last_cell = cell
+       rst.append(cell)
+       
+    conn.close()
+    
+    return rst
     
 
-  basic_cell = [BasicCell(k['name'],k['x'],k['y'],k['href'],k['style']) for k in c]
-
-  c.close()
+  
